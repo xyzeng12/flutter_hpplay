44e805056608ae6a48c94be526b23f5bbfb43c63
陈俊方 <chenjunfang@chenjunfangdeiMac.local>
2020-04-29 21:28:47 +0800

    更新

--- lib/flutter_hpplay.dart
+++ lib/flutter_hpplay.dart
@@ -19,6 +19,8 @@ class FlutterHpplay {
   List serviceNames = [];
   int currentIndex = -1;
   LBLelinkPlayStatus playStatus;
+  double duration;
+  double currentTime;
 
   DynamicHandler _onLelinkBrowserError;
   DynamicHandler _onLelinkBrowserDidFindLelinkServices;
@@ -131,6 +133,8 @@ class FlutterHpplay {
       },
       onLelinkPlayerProgressInfo: (Map<String, dynamic> message) async {
         _onLelinkPlayerProgressInfo(message);
+        duration = double.parse(message['duration']);
+        currentTime = double.parse(message['currentTime']);
         // print(
         //     'flutter--播放进度 总时长:${message['duration']}、当前播放位置:${message['currentTime']}');
       },
